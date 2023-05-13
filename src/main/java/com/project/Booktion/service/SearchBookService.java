149885dda9448aed46e3cb64e1cac4c1b50d35cb
Chaeyeon1475 <chaney2225@gmail.com>
2023-05-13 21:56:35 +0900

    13일 수정

--- src/main/java/com/project/Booktion/service/SearchBookService.java
+++ src/main/java/com/project/Booktion/service/SearchBookService.java
@@ -1,9 +1,10 @@
 package com.project.Booktion.service;
 
 import com.project.Booktion.model.Book;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class SearchBookService {
     public List<Book> search(String keyword) { return null;
     }

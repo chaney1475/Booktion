8b68877dbc9fdf386158f1405a618ab2ae62b4ba
rlawltn3551 <rlawltn3551@naver.com>
2023-05-19 22:05:23 +0900

    book controller 수정 및 book 관련 view 생성

--- src/main/java/com/project/Booktion/model/Review.java
+++ src/main/java/com/project/Booktion/model/Review.java
@@ -1,4 +1,6 @@
 package com.project.Booktion.model;
 
 public class Review {
+    public void setBook(Book book) {
+    }
 }
--- src/main/java/com/project/Booktion/service/ReviewService.java
+++ src/main/java/com/project/Booktion/service/ReviewService.java
@@ -1,11 +1,21 @@
 package com.project.Booktion.service;
 
+import com.project.Booktion.model.Book;
 import com.project.Booktion.model.Review;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ReviewService {
     public void write(Review review) {
+    }
+
+    public List<Review> getReviewsByBook(Book book) {
+        return null;
+    }
+
+    public void createReview(Review review) {
     }
 }

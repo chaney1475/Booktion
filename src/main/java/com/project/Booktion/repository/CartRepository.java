d293b9fba3f1e30ccf9059900c0e05771b75d331
chaechae118 <codbs43@naver.com>
2023-05-28 21:35:32 +0900

    repository, cart 컨트롤러

--- src/main/java/com/project/Booktion/repository/CartRepository.java
+++ src/main/java/com/project/Booktion/repository/CartRepository.java
@@ -1,4 +1,9 @@
 package com.project.Booktion.repository;
 
-public interface CartRepository {
+import com.project.Booktion.model.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    Cart findByClientId(Long clientId);
 }

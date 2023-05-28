ff258e1f97d9a31deb4f333e51aef1226f3f29a7
Chaeyeon1475 <chaney2225@gmail.com>
2023-05-28 21:55:04 +0900

    repository3번째수정

--- src/main/java/com/project/Booktion/repository/CartRepository.java
+++ src/main/java/com/project/Booktion/repository/CartRepository.java
@@ -1,11 +1,9 @@
 package com.project.Booktion.repository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface CartRepository {
 import com.project.Booktion.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+@Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
     Cart findByClientId(Long clientId);

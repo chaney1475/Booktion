74b7b3e2c617c0da1bc78a2dfb9ec7e3351200b5
gyungmean <yogurt2819@gmail.com>
2023-05-30 00:07:15 +0900

    used관련 수정
    bookRepository findByBookType메소드 추가
    orderRepository @Query오류 수정
    RegistUsedFormController requestmapping 오류 수정 / log코드추가
    SoldUsedController 메소드 인자 수정
    UsedBookRegist sellerId 타입 수정
    UsedBookRepository 메소드 명 수정
    UsedBookService 생성자 수정 / setOrderType 수정
    ViewUsedController return타입수정

--- src/main/java/com/project/Booktion/controller/usedBook/SoldUsedController.java
+++ src/main/java/com/project/Booktion/controller/usedBook/SoldUsedController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Slf4j // 로그 찍는 기능
@@ -19,9 +20,11 @@ public class SoldUsedController {
     private final UsedBookService usedBookService;
 
     @GetMapping("/myPage/used/sold")
-    public String getSellingUsedBooks(@PathVariable("memberId") String memberId, Model model) {
+    public String getSellingUsedBooks(HttpServletRequest request, Model model) {
         //판매완료된 중고책 목록을 가져온다.
-        model.addAttribute("soldUsedBooks", soldUsedBooks);
+        String userId = (String) request.getAttribute("userId");
+        List<Order> orderList = usedBookService.findOrderBySeller(userId);
+        model.addAttribute("soldUsedBooks", orderList);
         return "myPage/used/sold";
     }
 }

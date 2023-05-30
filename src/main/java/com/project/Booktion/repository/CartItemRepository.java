4ef5434c51ba830b012df7e2f7809776fbced1a5
Chaeyeon1475 <chaney2225@gmail.com>
2023-05-30 22:32:19 +0900

    cart 오류고침

--- src/main/java/com/project/Booktion/repository/CartItemRepository.java
+++ src/main/java/com/project/Booktion/repository/CartItemRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     CartItem findByCartIdAndCartItemId(Long cartId, Long cartItemId);
 
-    void deleteAllById(long cartItemId);
+    void deleteByCartId(long cartItemId);
 }

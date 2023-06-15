501d73d78b8069cc1e37fa37092bcf7fc539021a
Chaeyeon1475 <chaney2225@gmail.com>
2023-06-15 12:38:06 +0900

    화면 디자인 수정하고 검색기능 구현

--- src/main/java/com/project/Booktion/repository/BookRepository.java
+++ src/main/java/com/project/Booktion/repository/BookRepository.java
@@ -13,22 +13,12 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     Book findByBookIdAndBookType(long bookId, int bookType); // 북아이디로만 하면 중복이 있을 수 있어서. 북타입 조건 추가
 
     List<Book> findByBookType(int bookType);
+    List<Book> findByBookTypeAndTitleContaining(int bookType, String query);
 
     List<Book> findTop4ByBookTypeOrderByBookIdDesc(int bookType);
 
     List<Book> findTop4ByBookTypeAndPriceLessThanEqualOrderByBookIdDesc(int bookType, int price);
-//    List<Book> findByIsbn(String isbn); // isbn으로 검색 list로 할지 book으로 할지..
-//
-//    List<Book> findByBookType(int bookType); // 북타입으로 검색
-//
-//    List<Book> findByTitleContainingIgnoreCase(String keyword); // 제목에 특정 키워드가 포함된 책을 검색하는 메소드(대소문자 구분 없이)
-//
-//    List<Book> findByTitleContainingIgnoreCaseOrAuthorContainingIgnoreCaseOrPublisherContainingIgnoreCase(String title, String author, String publisher);
-//    // 제목이나 저자명이나 출판사에 특정 키워드가 포함된 책을 검색하는 메소드
-//    List<Book> findAllByOrderByTitle(); //제목순으로 검색
 
-//    List<Book> findAll(); // 모든 책을 검색하는 메소드
-
-//    long count();
+    List<Book> findByTitleContaining(String query);
 
 }
